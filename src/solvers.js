29a6dc351e79295a25cfeb763e7497d335218ce7
Hack Reactor Students <accounts@hackreactor.com>
2015-02-11 17:23:40 -0800

    The scaffolding is in place....

--- src/solvers.js
+++ src/solvers.js
@@ -13,11 +13,23 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+window.findNRooksSolution = function(n) {
+  var board = new Board({n:n});
+  var rows = board.rows();
+  var row;
+  for(var i=0; i<rows.length; i++) {
+    row = rows[i];
+    for(var j=0; j<row.length; j++) {
+      row[j] = 1;
+      if(board.hasRowConflictAt(i) || board.hasColConflictAt(j)) {
+        row[j] = 0;
+      }
+    }
+
+  }
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(rows));
+  return rows;
 };
 
 
@@ -25,7 +37,33 @@ window.findNRooksSolution = function(n) {
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = undefined; //fixme
+  var msolution = [];
+  var arg = [];
+  var board = new Board({n:n});
+  var rows = board.rows();
+  for(var i = 0; i<row.length; i++) {
+    cell = rows[0][i];
+    recursiveFn(cell);
+    }
+  }
+  var recursiveFn = function(arg){
+    if(arg.length === n){
+      solution.push(arg);
+    }
+    for (var k = 1; k < n; k++) {
+      for (var j = 0; j < rows.length; j++) {
+        row[k][j] = 1;
+        if(board.hasRowConflictAt(j) || board.hasColConflictAt(j)) {
+          row[k][j] = 0;
+          continue;
+        } else {
+          arg.push(row[k][j]);
+          recursiveFn(arg);
+          arg.pop()
+        }
+    }
 
+  }
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
